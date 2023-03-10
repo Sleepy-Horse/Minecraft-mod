01f6d25274e57e95d82e87a3ac584903e31f4b6c
Sleepy-Horse <97367938+Sleepy-Horse@users.noreply.github.com>
2023-03-10 23:27:36 +0100

    Added concentration effect via mixin and added support for mixins in the mod
    This commit implements a new feature in the mod that adds a concentration effect via a mixin. The effect highlights all mobs and players, making it easier to focus on specific targets. Additionally, the mod now supports mixins, allowing for more flexible customization and extension of its functionality.

--- src/main/java/smacho/macho/content/effects/Concentration.java
+++ src/main/java/smacho/macho/content/effects/Concentration.java
@@ -2,14 +2,11 @@ package smacho.macho.content.effects;
 
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
-import net.minecraft.world.entity.LivingEntity;
 
-public class Berserk extends MobEffect {
-    public Berserk(int color) {
+public class Concentration extends MobEffect {
+    public Concentration(int color) {
         super(MobEffectCategory.BENEFICIAL, color);
     }
-
-
 
     @Override
     public boolean isDurationEffectTick(int pDuration, int pAmplifier) {
